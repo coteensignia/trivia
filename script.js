1999452bb5aea00168bd504f9e1cd14c98444a4f
MARIA JOSE JOSE <77499159+coteensignia@users.noreply.github.com>
2023-03-23 01:04:25 +0000

    css

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+const register = document.getElementById("register") // llamar id register y trivia, revisando en todo el html, y se almacena en la constante y luego se llama abajo
+const trivia = document.getElementById("trivia")
+
+trivia.style.display = "none";
+//metodo style escribe en el html
+
 function start() {
   let nombre = document.getElementById("name").value
+  register.style.display = "none";
+  trivia.style.display = "block";
   alert(nombre + " " + "elige las dos alternativas correctas para ganar");
 }
+// al hacer click le pegamos los estilos para que se vea o no, llamando a las constantes de arriba
+
 let boton = document.getElementById("send")
 boton.addEventListener("click",send)
 function send() {
@@ -13,5 +23,5 @@ function send() {
 }
 
 // erfectooo lo entendi ....tenemos que meter esta funcion que la haga un bton....lo unico que cuandoncargas como que aparece entero yo creo que deberiamos hacerlo con el inner que dojeron ellos ademas por que... ese hace un recorrido por todo el html
-
+//crear otro boton para reiniciar el juego donde se vuelve a la pantalla donde se escribe el nombre
 //document.getElementById("flag").style.display = "none"
